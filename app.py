324d1c9929fc22c5fe47ba398ab58df5d7cab482
ENGR. SYED HASSAN IQBAL SHAH <48892265+ENGR28314@users.noreply.github.com>
2026-09-22 00:13:51 +0500

    Fix page_icon parameter in set_page_config

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import file_import
 
 st.set_page_config(
     page_title="Pakistan Water, Terrain & Disaster-Risk Explorer",
-    page_icon="🇵🇰", "Engr",
+    page_icon="🇵🇰",
     layout="wide",
     initial_sidebar_state="expanded",
 )
